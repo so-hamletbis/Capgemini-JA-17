8c7993402c47992392f9702ced5755c8a4fdd669
so-hamletbis <84120021+so-hamletbis@users.noreply.github.com>
2021-05-18 09:43:22 +0530

    Update Excercise1.java

--- CoreJava8-Labbook/Lab2/Excercise1.java
+++ CoreJava8-Labbook/Lab2/Excercise1.java
@@ -1,36 +1,23 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Excercise1 {
-	public static int getSecondSmallest(int[] arr)
-	{
-		for(int i=0; i<arr.length; i++)
-		{
-			for(int j=1; j<arr.length-i; j++)
-			{
-				if(arr[j-1] > arr[j])
-				{
-					int temp = arr[j-1];
-					arr[j-1] = arr[j];
-					arr[j] = temp;
-				}
-			}
-		}
+	public static int getSecondSmallest(int[] arr) {
+		Arrays.sort(arr);
 		return arr[1];
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter length of array:");
-		int len = sc.nextInt();
-		int arr[] = new int[len];
-		System.out.println("Enter the array values:");
-		for(int i=0; i<len; i++)
-		{
-			arr[i] = sc.nextInt();
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter number of elements in array:");
+		int n=sc.nextInt();
+		System.out.println("Enter array elements:");
+		int array[]=new int[n];
+		for(int i=0;i<n;i++) {
+			array[i]=sc.nextInt();
 		}
-		int val = getSecondSmallest(arr);
-		System.out.println("Second smallest element in the array: "+ val);
+		System.out.println("The second smallest element is " + getSecondSmallest(array));
 		sc.close();
 	}
 
