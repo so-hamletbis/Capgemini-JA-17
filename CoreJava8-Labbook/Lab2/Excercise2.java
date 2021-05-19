51ded02dc57ccb0194272af0bd877c78688c30c6
so-hamletbis <84120021+so-hamletbis@users.noreply.github.com>
2021-05-19 12:12:47 +0530

    Update Excercise2.java

--- CoreJava8-Labbook/Lab2/Excercise2.java
+++ CoreJava8-Labbook/Lab2/Excercise2.java
@@ -1,34 +1,27 @@
-import java.util.*;
-public class Excercise2 {
+import java.util.Scanner;
+import java.util.Arrays;
 
-	static String[] sortStrings(String[] arr) {
-		Arrays.sort(arr);
-		int len = arr.length;
-		String[] newArr = new String[len];
-		for (int i = 0; i < (int)Math.ceil(len/2)+1; i++)
-			newArr[i] = arr[i].toUpperCase();
-		for (int i = (int)Math.ceil(len/2)+1; i < len; i++)
-			newArr[i] = arr[i].toLowerCase();
-		return newArr;
-	}
+public class Excercise2 {
+	public static String[] sortStrings(String[] array) {
+		Arrays.sort(array);
+		return array;
+		}
 	
 	public static void main(String[] args) {
-		Scanner sc1 = new Scanner(System.in);
-		Scanner sc2 = new Scanner(System.in);
-		
-		int num = sc1.nextInt();
-		String entries[] = new String[num];
-		for(int i=0; i<num; i++){
-			entries[i] = sc2.nextLine();
-        }
-		System.out.println("The resultant array is: ");
-		String[] sortArray = sortStrings(entries);
-		for(String value : sortArray){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number of characters:");
+		int n=sc.nextInt();
+		System.out.println("Enter elements in array:");
+		String arr[]=new String[n];
+		for(int i=0;i<arr.length;i++) {
+			arr[i]=sc.next();
+		}
+		String[] val = sortStrings(arr);
+		for(String value : val) {
 			System.out.println(value);
-        }
-		sc1.close();
-		sc2.close();
+		}
 		
+		sc.close();
 	}
 
 }
