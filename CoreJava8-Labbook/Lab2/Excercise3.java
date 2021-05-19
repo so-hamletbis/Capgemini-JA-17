fb047eb7f4578d7a4ae97bb9a5653576b56013ed
so-hamletbis <84120021+so-hamletbis@users.noreply.github.com>
2021-05-19 12:13:22 +0530

    Update Excercise3.java

--- CoreJava8-Labbook/Lab2/Excercise3.java
+++ CoreJava8-Labbook/Lab2/Excercise3.java
@@ -1,27 +1,32 @@
 import java.util.*;
-public class Excercise3 {
 
-	static int[] getSorted(int[] arr) {
-		int len = arr.length;
+public class Excercise3 {
+	public static int[] getSorted(int array[]) {
+		int len = array.length;
 		for (int i = 0; i < len; i++) {
-			StringBuffer s = new StringBuffer(String.valueOf(arr[i]));
+			StringBuffer s = new StringBuffer(String.valueOf(array[i]));
 			s.reverse();
-			arr[i] = Integer.parseInt(String.valueOf(s));
+			array[i] = Integer.parseInt(String.valueOf(s));
 		}
-		Arrays.sort(arr);
-		return arr;
+		Arrays.sort(array);
+		return array;
 	}
 	
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		
-		int n = sc.nextInt();
-		int[] arr = new int[n];
-		for (int i = 0; i < n; i++)
-			arr[i] = sc.nextInt();
-		int[] ans = getSorted(arr);
-		System.out.println(Arrays.toString(ans));
-		
+		System.out.println("Enter number of elements in array:");
+		int n=sc.nextInt();
+		int arr[]=new int[n];
+		System.out.println("Enter the elements:");
+		for(int i=0;i<n;i++) {
+			arr[i]=sc.nextInt();
+		}
+		int newarr[]= getSorted(arr);
+		System.out.println("The resultant array:");
+		for(int i=0; i<newarr.length;i++) {
+			System.out.println(newarr[i]);
+		}
 		sc.close();
 	}
 
