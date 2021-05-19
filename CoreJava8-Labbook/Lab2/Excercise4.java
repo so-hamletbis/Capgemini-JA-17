d12db8740b85ab4281c6af42e9a19da0d2168829
so-hamletbis <84120021+so-hamletbis@users.noreply.github.com>
2021-05-19 12:13:52 +0530

    Update Excercise4.java

--- CoreJava8-Labbook/Lab2/Excercise4.java
+++ CoreJava8-Labbook/Lab2/Excercise4.java
@@ -1,8 +1,9 @@
-import java.util.*;
-public class Excercise4 {
+import java.util.Scanner;
+import java.util.Arrays;
 
-	static int[] modifyArray(int[] arr) {
-		int n = arr.length;
+public class Excercise4 {
+	public static int[] modifyArray(int array[]) {
+		int n = array.length;
 		boolean[] visited = new boolean[n];
 		for (int i = 0; i < n; i++) {
 			visited[i] = true;
@@ -11,7 +12,7 @@ public class Excercise4 {
 		for (int i = 0; i < n; i++) {
 			if (visited[i] == true) {
 				for (int j = i+1; j < n; j++) {
-					if (arr[i] == arr[j]) {
+					if (array[i] == array[j]) {
 						visited[j] = false;
 						ct += 1;
 					}
@@ -22,7 +23,7 @@ public class Excercise4 {
 		int[] ans = new int[n-ct];
 		for (int i = 0; i < visited.length; i++) {
 			if (visited[i] == true) {
-				ans[k] = arr[i];
+				ans[k] = array[i];
 				k += 1;
 			}
 		}
@@ -38,16 +39,18 @@ public class Excercise4 {
 		}
 		return ans;
 	}
-	
+
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int n = sc.nextInt();
-		int arr[] = new int[n];
-		for (int i = 0; i < n; i++)
-			arr[i] = sc.nextInt();
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter number of elements in array:");
+		int n=sc.nextInt();
+		System.out.println("Enter the elements:");
+		int arr[]=new int[n];
+		for(int i=0;i<n;i++) {
+			arr[i]=sc.nextInt();
+		}
 		System.out.println(Arrays.toString(modifyArray(arr)));
-		
 		sc.close();
 	}
 
